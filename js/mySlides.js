f28d93befaad9ce03a6799413caf8a31fa73eb04
ShehabMuhammad <47852641+ShehabMuhammad@users.noreply.github.com>
2021-09-19 17:14:06 +0200

    Update mySlides.js

--- js/mySlides.js
+++ js/mySlides.js
@@ -1,18 +1,22 @@
+// The images we want to show in the slide show, We can add more, It's helpful to put them in an array..
 const images = ["A_blue_sky11.jfif",
 "acapulco-1545208.jpg",
 "ocean-view-1311053-1280x960.jpg","sun-by-the-trees-1544340.jpg"], base = "images/";
 
+// We grab the element that contains the image..
 const imgHolder = document.getElementById("imageHolder");
+
+// The index of the image, And a boolean value indication whether to automate the slideshow or not..
 let index = 3, auto = true;
 function nextSlide(){
     index++;
     if( index >= images.length)index=0;
     imgHolder.setAttribute("src", base + images[index]);
     imgHolder.animate([
-  { // from
+  { 
     opacity: 0
   },
-  { // to
+  { 
     opacity: 1
   }
 ], {duration:1400, iterations:1} );
